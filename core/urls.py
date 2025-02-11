554370a689a0dba710426bf0e1ef5301803497e2
Ammar <ammar.torky28@gmail.com>
2025-02-11 15:52:00 +0200

    V2 using Class Based View (CBV)

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,11 @@ from .views import *
 
 urlpatterns = [
     path('',index),
-    path('all/',alltodos,name='alltodos'),
-    path('add/',New_ToDo,name = 'new_todo'),
+    path('all/',alltodos.as_view(),name='alltodos'),
+    path('add/',New_ToDo.as_view(),name = 'new_todo'),
+    path('detail/<int:pk>/', Detail_ToDo.as_view(), name='todo_detail'),
+    path('edit/<int:pk>/', TodoUpdateView.as_view(), name='edit_todo'),
+    path('delete/<int:pk>/', TodoDeleteView.as_view(), name='delete_todo')
+
+    
 ]
--- core/views.py
+++ core/views.py
@@ -1,24 +1,44 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .forms import *
+from django.views.generic.edit import CreateView , UpdateView, DeleteView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
 
-def alltodos(request ):
-    # variable for all items
-    all_items = Todo.objects.all().order_by("-id")
-    return render(request,'alltodos.html',{'all_items' : all_items})
+class alltodos(ListView):
+    model = Todo
+    template_name = 'alltodos.html'
+    paginate_by = 10
+    def get_queryset(self):
+        return Todo.objects.all().order_by('-id')
 
-def New_ToDo(request):
-    if request.method == 'GET':
-        form = AddNewTodo()
-        return render(request, 'newtodos.html', {'form': form})
-    elif request.method == 'POST':
-        form = AddNewTodo(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('alltodos')
-        
+
+class New_ToDo(CreateView):
+    model = Todo
+    fields = ['text']
+    template_name = 'newtodos.html'
+    #the forward link after complete
+    success_url = '/all/'
+
+class Detail_ToDo(DetailView):
+    model = Todo
+    template_name = 'todo_detail.html'
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ['text']
+    template_name = 'updateToDo.html'
+    #the forward link after complete
+    success_url = '/all/'
+
+class TodoDeleteView(DeleteView):
+    model = Todo
+    template_name = 'deleteToDo.html'
+    success_url = '/all/'
